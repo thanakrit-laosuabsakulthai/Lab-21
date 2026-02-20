1da8f1e72623eb4577bbf73d9b20d71cc6645ce6
Thanakrit <thanakrit_la@cmu.ac.th>
2026-02-20 16:57:52 +0700

    lab completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cstdlib>
 
+int main(int argument_count, char* argument_values[]) {
+	if (argument_count > 1) {
+		double sum = 0.0;
+		for (int i = 1; i < argument_count; ++i) {
+			sum += std::atof(argument_values[i]);
+		}
+		
+		double average = sum / (argument_count - 1);
+		std::cout << "---------------------------------" << std::endl;
+		std::cout << "Average of " << (argument_count - 1) << " numbers = " << average << std::endl;
+		std::cout << "---------------------------------" << std::endl;
+	} else {
+		std::cout << "Please input numbers to find average." << std::endl;
+	}
+	
+	
+	return 0;
+}
--- calculator.cpp
+++ calculator.cpp
@@ -1,8 +1,133 @@
 #include <windows.h>
+#include <cstdlib>
+#include <cstdio>
+
+HWND mytext, button1 , input1, input2, button2, button3, button4;
+char firstinput[100], secondinput[100];
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
+		
+		case WM_CREATE: {
+			mytext = CreateWindowEx(0, "STATIC",
+				"Please input two numbers",
+				WS_CHILD | WS_VISIBLE,
+				30, 20, 200, 25,
+				hwnd, NULL, NULL, NULL
+			);
+			button1 = CreateWindowEx(0, "BUTTON",
+				"+",
+				WS_CHILD | WS_VISIBLE | WS_BORDER,
+				70, 105, 20, 20,
+				hwnd, (HMENU) 1, NULL, NULL
+			);
+			button2 = CreateWindowEx(0, "BUTTON",
+				"-",
+				WS_CHILD | WS_VISIBLE | WS_BORDER,
+				95, 105, 20, 20,
+				hwnd, (HMENU) 2, NULL, NULL
+			);
+			button3 = CreateWindowEx(0, "BUTTON",
+				"*",
+				WS_CHILD | WS_VISIBLE | WS_BORDER,
+				120, 105, 20, 20,
+				hwnd, (HMENU) 3, NULL, NULL
+			);
+			button4 = CreateWindowEx(0, "BUTTON",
+				"/",
+				WS_CHILD | WS_VISIBLE | WS_BORDER,
+				145, 105, 20, 20,
+				hwnd, (HMENU) 4, NULL, NULL
+			);
+			input1 = CreateWindowEx(0, "EDIT",
+				"",
+				WS_CHILD | WS_VISIBLE,
+				40, 50, 160, 20,
+				hwnd, NULL, NULL, NULL
+			);
+			input2 = CreateWindowEx(0, "EDIT",
+				"",
+				WS_CHILD | WS_VISIBLE,
+				40, 75, 160, 20,
+				hwnd, NULL, NULL, NULL
+			);
+			
+			break;
+		}
+		
+		case WM_COMMAND: {
+			switch(LOWORD(wParam)) {
+				case 1: {
+					
+					GetWindowText(input1, firstinput, 100);
+					GetWindowText(input2, secondinput, 100);
+					// plus 5
+					float firstnum = std::atof(firstinput);
+					float secondnum = std::atof(secondinput);
+					float result = firstnum + secondnum;
+					char resulttext[100];
+					std::sprintf(resulttext, "%f", result);
+					
+					::MessageBox(hwnd, resulttext, "Result", MB_OK);
+					break;
+				}
+				case 2: {
+					
+					GetWindowText(input1, firstinput, 100);
+					GetWindowText(input2, secondinput, 100);
+					// minus 5
+					float firstnum = std::atof(firstinput);
+					float secondnum = std::atof(secondinput);
+					float result = firstnum - secondnum;
+					char resulttext[100];
+					std::sprintf(resulttext, "%f", result);
+					
+					::MessageBox(hwnd, resulttext, "Result", MB_OK);
+					break;
+				}
+				
+				case 3: {
+					
+					GetWindowText(input1, firstinput, 100);
+					GetWindowText(input2, secondinput, 100);
+					// times 5
+					float firstnum = std::atof(firstinput);
+					float secondnum = std::atof(secondinput);
+					float result = firstnum * secondnum;
+					char resulttext[100];
+					std::sprintf(resulttext, "%f", result);
+					
+					::MessageBox(hwnd, resulttext, "Result", MB_OK);
+					break;
+				}
+				
+				case 4: {
+					
+					GetWindowText(input1, firstinput, 100);
+					GetWindowText(input2, secondinput, 100);
+					// divide 5
+					float firstnum = std::atof(firstinput);
+					float secondnum = std::atof(secondinput);
+					if (secondnum == 0) {
+						::MessageBox(hwnd, "Cannot divide by zero!", "Result", MB_OK | MB_ICONERROR);
+						break;
+					}
+					float result = firstnum / secondnum;
+					char resulttext[100];
+					std::sprintf(resulttext, "%f", result);
+					
+					::MessageBox(hwnd, resulttext, "Result", MB_OK);
+					break;
+					
+				}
+			}
+
+			break;
+			
+			
+		}
+		
 		
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
@@ -31,7 +156,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = CreateSolidBrush(RGB(120, 120, 240));
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,11 +166,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
